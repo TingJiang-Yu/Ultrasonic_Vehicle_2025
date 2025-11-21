e1cb9cd27a23f79383fb6ceba8b52da5a624c58a
TingJiang_Yu <longlong51226@qq.com>
2025-11-21 15:46:48 +0800

    nope, not today

--- README.md
+++ README.md
@@ -2,13 +2,24 @@
 
 ## 使用方法：
 
-打开`Ultrasonic_Vehicle_2025`文件夹，打开`src`文件夹，可以看到所有的文件
+使用`vscode`打开`Ultrasonic_Vehicle_2025`文件夹，在`src`文件夹中，可以看到所有的文件。
 
-目前搭建了一个基础的框架，但还要根据实际情况更改
+目前搭建了一个基础的框架，但还要根据实际情况更改。
 
 大体的思路：
-- `main_controller`作为上层控制，直接调用底层库函数，采集底层的信息，处理信息并产生命令，有点类似于`int main()`函数
-  - `motor_controller`作为电机控制板底层，负责驱动电机（如果有现成的库，就不用这个了）
-  - `pid_controller`作为$PID$算法库，如果需要就用
-  - `ultrasonic_controller`作为超声波模块底层，负责驱动超声波模块
+- `main_controller`作为上层控制，直接调用底层库函数，采集底层的信息，处理信息并产生命令，有点类似于`int main()`函数。
+  - `motor_controller`作为电机控制板底层，负责驱动电机（如果有现成的库，就不用这个了）。
+  - `pid_controller`作为$PID$算法库，如果需要就用。
+  - `ultrasonic_controller`作为超声波模块底层，负责驱动超声波模块。
 
+## tips:
+
+提交代码的时候，在`vscode`中右键点击`Ultrasonic_Vehicle_2025`文件夹，选择“在集成终端中打开”选项。
+
+在终端输入
+```bash
+git add .
+git commit -m "anything you want to say!"
+```
+
+然后快捷键`Ctrl+Shift+G`打开源代码管理，点击提交，这里如果报错就关掉梯子，然后重新提交就可以看到`github`上面更新了你的代码。
--- Ultrasonic_Vehicle_2025/src/pid_controller.cpp
+++ Ultrasonic_Vehicle_2025/src/pid_controller.cpp
@@ -1,1 +1,68 @@
 #include "pid_controller.h"
+
+pid_controller::pid_controller(float* data_d, float* data_o)
+{
+
+    this->obs = new pid_data();
+    this->data_d = data_d;
+    this->data_o = data_o;
+}
+
+pid_controller::pid_controller()
+{
+    this->obs = new pid_data();
+}
+
+bool pid_controller::Calc(float data_t)
+{
+    error = *data_d - data_t;      // 计算误差
+    obs->e[2] = error - obs->e[0]; // 离散微分
+    obs->e[1] += error;            // 积分
+    obs->e[0] = error;             // 更新误差
+
+    // 积分限幅
+    if (obs->e[1] > obs->l[0])
+        obs->e[1] = obs->l[0];
+    else if (obs->e[1] < -obs->l[0])
+        obs->e[1] = -obs->l[0];
+    
+    // 临时数据容器，计算PID原始值
+    data_ = obs->k[0] * obs->e[0] + 
+            obs->k[1] * obs->e[1] + 
+            obs->k[2] * obs->e[2];
+    
+    // 输出限幅
+    if (data_ > obs->l[2])
+        *data_o = obs->l[2];
+    else if (data_ < obs->l[1])
+        *data_o = obs->l[1];
+    else
+        *data_o = data_;
+
+    return true;
+}
+
+bool pid_controller::SetD(float data_d)
+{
+    *this->data_d = data_d;
+
+    return true;
+}
+
+bool pid_controller::SetK(float kp, float ki, float kd)
+{
+    this->obs->k[0] = kp;
+    this->obs->k[1] = ki;
+    this->obs->k[2] = kd;
+
+    return true;
+}
+
+bool pid_controller::SetL(float l_int, float l_min, float l_max)
+{
+    this->obs->l[0] = l_int;
+    this->obs->l[1] = l_min;
+    this->obs->l[2] = l_max;
+
+    return true;
+}
