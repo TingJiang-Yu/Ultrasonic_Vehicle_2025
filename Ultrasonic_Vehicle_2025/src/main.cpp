3d55b03be59641c34f631539609776d560123913
TingJiang_Yu <longlong51226@qq.com>
2025-12-22 20:21:52 +0800

    临时的超声波测试代码

--- Ultrasonic_Vehicle_2025/src/main.cpp
+++ Ultrasonic_Vehicle_2025/src/main.cpp
@@ -2,14 +2,20 @@
 #include "main_controller.h"
 
 main_controller ctrl;
+ultrasonic_controller usc;
 
 void setup() 
 {
     Serial.begin(SERIAL_BAUDRATE);
-    ctrl.init();
+    usc.init();
+    // ctrl.init();
 }
 
 void loop() 
 {
-    ctrl.update();
+    long timeDiff = usc.getTimeDiff();
+    Serial.print("Time diff: ");
+    Serial.println(timeDiff);
+    Serial.println("mus");
+    // ctrl.update();
 }
